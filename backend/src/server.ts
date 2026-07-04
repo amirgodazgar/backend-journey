b21b6639fb16b569c11fdf64799dc9224506284a
Amir Godazgar <amir.godazgar@connect2wow.com>
2026-07-04 12:14:01 +0330

    #Backend
    - update route handling

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,37 +1,51 @@
 import { createServer } from "node:http";
 
+const products = [
+  { id: 1, title: "Book" },
+  { id: 2, title: "Pen" },
+  { id: 3, title: "Eraser" },
+  { id: 4, title: "Notebook" },
+];
+
 const HOST_NAME = "127.0.0.1";
 const PORT = 3000;
 
 const server = createServer((req, res) => {
+  const url = new URL(req.url!, `http://${req.headers.host}`);
+  const method = req.method;
+  const match = url.pathname.match(/^\/products\/(\d+)$/);
 
-  if (req.url === "/products" && req.method === "GET") {
+  if (url.pathname === "/products" && method === "GET") {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify([
-        { id: 1, title: "Book" },
-        { id: 2, title: "Pen" },
-      ]),
-    );
-    
-  } else if (req.url === "/update" && req.method === "POST") {
+    res.end(JSON.stringify(products));
+  } else if (match && method === "GET") {
+    const id = Number(match[1]);
+    const item = products.find((i) => i.id === id);
+    if (!item) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product not found" }));
+      return; //* ← important: stop execution
+    }
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(item));
+  } else if (url.pathname === "/products" && method === "POST") {
     let body = "";
-
     //* Listen for 'first chunk' of data to be received from the request body
     req.on("data", (chunk) => {
       body += chunk.toString();
     });
     // * Listen for the end of the request body ('all chunks' have been received)
     req.on("end", () => {
-      const responseBody = JSON.parse(body);
-      responseBody.products = [
-        { id: 1, title: "Book" },
-        { id: 2, title: "Pen" },
-      ];
-      res.writeHead(201, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(responseBody));
+      try {
+        const responseBody = JSON.parse(body);
+        res.writeHead(201, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(responseBody));
+      } catch (error) {
+        console.log("Error in server");
+        res.writeHead(400, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Invalid JSON" }));
+      }
     });
-    
   } else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ data: "Not Found" }));
